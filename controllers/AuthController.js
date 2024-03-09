9d871f6ce2def27a77f1e1384bc8335409fb3f3c
niyioo <adeniyi@cybersecinsight.com>
2024-03-09 18:25:57 +0100

    auth

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -30,7 +30,7 @@ export default class AuthController {
       const token = uuidv4();
       const key = `auth_${token}`;
 
-      await redisClient.set(key, user._id.toString(), 'EX', 24 * 60 * 60);
+      await redisClient.set(key, user._id.toString(), 24 * 60 * 60 * 1000);
 
       return response.status(200).json({ token });
     } catch (error) {
